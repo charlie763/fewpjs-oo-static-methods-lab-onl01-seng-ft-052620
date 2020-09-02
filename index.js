a0b6a2d2fa9f2c9f2dd1f5693db25f79cbbe4674
charlie763 <cwisoff@gmail.com>
2020-09-02 15:21:41 -0600

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string){
+    return string[0].toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(str){
+    return str.replace(/[^A-Za-z0-9-\s']+/g, '');
+  }
+
+  static titleize(str){
+    const badWords = ["the", 'a', "an", "but", "of", "and", "for", "at", "by", "from"];
+    return str.split(" ").map(function(word, i){
+      if (badWords.includes(word) && i !== 0){
+        return word;
+      } else {
+        return this.capitalize(word);
+      }
+    }.bind(this)).join(" ")
+  }
 }
